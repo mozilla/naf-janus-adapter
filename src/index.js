72dc4be38f71327898f7d27c539f42b9f67782bc
Marshall Quander <marshall@quander.me>
2018-02-02 14:52:38 -0800

    Upgrade minijanus, improve Janus connection code (#18)
    * Simplify, correctify Janus connection code
    
    * Simplify some connection tracking code
    
    * Bump minijanus to 0.4.0
    
    * Update build artifacts
    
    * Update example to test audio.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 var mj = require("minijanus");
 var debug = require("debug")("naf-janus-adapter:debug");
+var warn = require("debug")("naf-janus-adapter:warn");
+var error = require("debug")("naf-janus-adapter:error");
 
 function randomUint() {
   return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-}
-
-function waitForEvent(target, event) {
-  return new Promise((resolve, reject) => {
-    target.addEventListener(event, e => resolve(e), { once: true });
-  });
 }
 
 const PEER_CONNECTION_CONFIG = {
@@ -30,7 +26,6 @@ class JanusAdapter {
 
     this.publisher = null;
     this.occupants = {};
-    this.occupantPeerConnections = {};
     this.mediaStreams = {};
     this.pendingMediaRequests = new Map();
 
@@ -40,8 +35,6 @@ class JanusAdapter {
 
     this.onWebsocketMessage = this.onWebsocketMessage.bind(this);
     this.onDataChannelMessage = this.onDataChannelMessage.bind(this);
-
-    this.webRtcUpPromises = new Map();
   }
 
   setServerUrl(url) {
@@ -96,57 +89,22 @@ class JanusAdapter {
     // One reliable datachannel and one unreliable.
     this.publisher = await this.createPublisher();
 
-    this.mediaStreams[this.userId] = this.publisher.mediaStream;
-
-    // Resolve the promise for the user's media stream if it exists.
-    if (this.pendingMediaRequests.has(this.userId)) {
-      this.pendingMediaRequests
-        .get(this.userId)
-        .resolve(this.publisher.mediaStream);
-    }
+    this.setMediaStream(this.userId, this.publisher.mediaStream);
 
     // Add all of the initial occupants.
-    for (let occupantId of this.publisher.initialOccupants) {
-      if (occupantId !== this.userId) {
-        this.addOccupant(occupantId);
-      }
-    }
+    await Promise.all(this.publisher.initialOccupants.map(this.addOccupant.bind(this)));
   }
 
   onWebsocketMessage(event) {
-    var message = JSON.parse(event.data);
-    this.session.receive(message);
-
-    // Handle all of the join and leave events from the publisher.
-    if (message.plugindata && message.plugindata.data) {
-      var data = message.plugindata.data;
-      if (data.event === "join" && data.room_id === this.room) {
-        this.addOccupant(data.user_id);
-      } else if (
-        data.event &&
-        data.event === "leave" &&
-        data.room_id === this.room
-      ) {
-        this.removeOccupant(data.user_id);
-      }
-    }
-
-    if (message.janus && message.janus === "webrtcup") {
-      this.getWebRtcUpPromise(message.sender).resolve();
-    }
+    this.session.receive(JSON.parse(event.data));
   }
 
   async addOccupant(occupantId) {
     var subscriber = await this.createSubscriber(occupantId);
 
-    this.occupants[occupantId] = true;
-    this.occupantPeerConnections[occupantId] = subscriber.peerConnection;
-    this.mediaStreams[occupantId] = subscriber.mediaStream;
+    this.occupants[occupantId] = subscriber;
 
-    // Resolve the promise for the user's media stream if it exists.
-    if (this.pendingMediaRequests.has(occupantId)) {
-      this.pendingMediaRequests.get(occupantId).resolve(subscriber.mediaStream);
-    }
+    this.setMediaStream(occupantId, subscriber.mediaStream);
 
     // Call the Networked AFrame callbacks for the new occupant.
     this.onOccupantConnected(occupantId);
@@ -158,9 +116,9 @@ class JanusAdapter {
   removeOccupant(occupantId) {
     if (this.occupants[occupantId]) {
       // Close the subscriber peer connection. Which also detaches the plugin handle.
-      if (this.occupantPeerConnections[occupantId]) {
-        this.occupantPeerConnections[occupantId].close();
-        delete this.occupantPeerConnections[occupantId];
+      if (this.occupants[occupantId]) {
+        this.occupants[occupantId].conn.close();
+        delete this.occupants[occupantId];
       }
 
       if (this.mediaStreams[occupantId]) {
@@ -176,22 +134,39 @@ class JanusAdapter {
         this.pendingMediaRequests.delete(occupantId);
       }
 
-      delete this.occupants[occupantId];
-
       // Call the Networked AFrame callbacks for the removed occupant.
       this.onOccupantDisconnected(occupantId);
       this.onOccupantsChanged(this.occupants);
     }
   }
 
-  getWebRtcUpPromise(id) {
-    if (!this.webRtcUpPromises.get(id)) {
-      const promise = new Promise((resolve, reject) => {
-        this.webRtcUpPromises.set(id, { resolve });
+  negotiateIce(conn, handle) {
+    return new Promise((resolve, reject) => {
+      conn.addEventListener("icecandidate", ev => {
+        handle.sendTrickle(ev.candidate || null).then(() => {
+          if (!ev.candidate) { // this was the last candidate on our end and now they received it
+            resolve();
+          }
+        }, reject);
       });
-      this.webRtcUpPromises.get(id).promise = promise;
-    }
-    return this.webRtcUpPromises.get(id);
+    });
+  }
+
+  async negotiatePublisherMedia(conn, handle) {
+    debug("pub sending offer and setting remote/local description");
+    var offer = await conn.createOffer();
+    var localReady = conn.setLocalDescription(offer);
+    var remoteReady = handle.sendJsep(offer).then(({ jsep }) => conn.setRemoteDescription(jsep));
+    return await Promise.all([localReady, remoteReady]);
+  }
+
+  async negotiateSubscriberMedia(conn, handle, offer) {
+    debug("sub sending answer and setting remote/local description");
+    var desc = await conn.setRemoteDescription(offer);
+    var answer = await conn.createAnswer();
+    var localReady = conn.setLocalDescription(answer);
+    var remoteReady = handle.sendJsep(answer);
+    return await Promise.all([localReady, remoteReady]);
   }
 
   async createPublisher() {
@@ -199,67 +174,49 @@ class JanusAdapter {
     debug("pub waiting for sfu");
     await handle.attach("janus.plugin.sfu");
 
-    var peerConnection = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
+    var conn = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
 
-    peerConnection.addEventListener("icecandidate", event => {
-      handle.sendTrickle(event.candidate || null);
-    });
+    this.negotiateIce(conn, handle).catch(err => error("Error negotiating ICE candidates: %o", err));
 
     // Create an unreliable datachannel for sending and receiving component updates, etc.
-    var unreliableChannel = peerConnection.createDataChannel("unreliable", {
-      ordered: false,
-      maxRetransmits: 0
-    });
+    var unreliableChannel = conn.createDataChannel("unreliable", { ordered: false, maxRetransmits: 0 });
     unreliableChannel.addEventListener("message", this.onDataChannelMessage);
 
     // Create a reliable datachannel for sending and recieving entity instantiations, etc.
-    var reliableChannel = peerConnection.createDataChannel("reliable", {
-      ordered: true
-    });
+    var reliableChannel = conn.createDataChannel("reliable", { ordered: true });
     reliableChannel.addEventListener("message", this.onDataChannelMessage);
 
     var mediaStream;
     // @TODO either this should wait or setLocalMediaStream should renegotiate (or both)
     if (this.localMediaStream) {
       mediaStream = this.localMediaStream;
-      peerConnection.addStream(this.localMediaStream);
+      conn.addStream(this.localMediaStream);
     } else {
-      console.warn("localMediaStream not set. Will not publish audio or video");
+      warn("localMediaStream not set. Will not publish audio or video");
     }
 
-    debug("pub waiting for offer");
-    var offer = await peerConnection.createOffer();
-
-    debug("pub waiting for local/remote descriptions");
-
-    await Promise.all([
-      peerConnection.setLocalDescription(offer),
-      handle
-        .sendJsep(offer)
-        .then(({ jsep }) => peerConnection.setRemoteDescription(jsep))
-    ]);
+    this.negotiatePublisherMedia(conn, handle).catch(err => error("Error negotiating media: %o", err));
 
     debug("pub waiting for webrtcup");
-    await this.getWebRtcUpPromise(handle.id).promise;
-
-    if (reliableChannel.readyState !== "open") {
-      debug("pub waiting for channel to open");
-      // Wait for the reliable datachannel to be open before we start sending messages on it.
-      await waitForEvent(reliableChannel, "open");
-    }
+    await new Promise(resolve => handle.on("webrtcup", resolve));
 
     // Call the naf connectSuccess callback before we start receiving WebRTC messages.
     this.connectSuccess(this.userId);
 
-    debug("pub waiting for join");
-    // Send join message to janus. Listen for join/leave messages. Automatically subscribe to all users' WebRTC data.
-    var message = await this.sendJoin(handle, this.room, this.userId, {
-      notifications: true,
-      data: true
+    // Handle all of the join and leave events.
+    handle.on("event", ev => {
+      var data = ev.plugindata.data;
+      if (data.event == "join" && data.room_id == this.room) {
+        this.addOccupant(data.user_id);
+      } else if (data.event == "leave" && data.room_id == this.room) {
+        this.removeOccupant(data.user_id);
+      }
     });
 
-    var initialOccupants =
-      message.plugindata.data.response.users[this.room] || [];
+    debug("pub waiting for join");
+    // Send join message to janus. Listen for join/leave messages. Automatically subscribe to all users' WebRTC data.
+    var message = await this.sendJoin(handle, { notifications: true, data: true });
+    var initialOccupants = message.plugindata.data.response.users[this.room] || [];
 
     debug("publisher ready");
     return {
@@ -268,8 +225,23 @@ class JanusAdapter {
       reliableChannel,
       unreliableChannel,
       mediaStream,
-      peerConnection
+      conn
     };
+  }
+
+  configureSubscriberSdp(originalSdp) {
+    // TODO: Hack to get video working on Chrome for Android. https://groups.google.com/forum/#!topic/mozilla.dev.media/Ye29vuMTpo8
+    if (navigator.userAgent.indexOf("Android") === -1) {
+      return originalSdp.replace(
+        "a=rtcp-fb:107 goog-remb\r\n",
+        "a=rtcp-fb:107 goog-remb\r\na=rtcp-fb:107 transport-cc\r\na=fmtp:107 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f\r\n"
+      );
+    } else {
+      return originalSdp.replace(
+        "a=rtcp-fb:107 goog-remb\r\n",
+        "a=rtcp-fb:107 goog-remb\r\na=rtcp-fb:107 transport-cc\r\na=fmtp:107 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f\r\n"
+      );
+    }
   }
 
   async createSubscriber(occupantId) {
@@ -277,70 +249,37 @@ class JanusAdapter {
     debug("sub waiting for sfu");
     await handle.attach("janus.plugin.sfu");
 
-    var peerConnection = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
+    var conn = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
 
-    peerConnection.addEventListener("icecandidate", event => {
-      handle.sendTrickle(event.candidate || null);
-    });
+    this.negotiateIce(conn, handle).catch(err => error("Error negotiating ICE candidates: %o", err));
 
     debug("sub waiting for join");
     // Send join message to janus. Don't listen for join/leave messages. Subscribe to the occupant's audio stream.
-    const resp = await this.sendJoin(handle, this.room, this.userId, {
-      notifications: false,
-      media: occupantId
-    });
+    const resp = await this.sendJoin(handle, { media: occupantId });
+    resp.jsep.sdp = this.configureSubscriberSdp(resp.jsep.sdp);
 
-    debug("sub waiting for answer");
-    let sdp = resp.jsep.sdp;
-
-    // TODO: Hack to get video working on Chrome for Android. https://groups.google.com/forum/#!topic/mozilla.dev.media/Ye29vuMTpo8
-    if (navigator.userAgent.indexOf("Android") === -1) {
-      sdp = sdp.replace(
-        "a=rtcp-fb:107 goog-remb\r\n",
-        "a=rtcp-fb:107 goog-remb\r\na=rtcp-fb:107 transport-cc\r\na=fmtp:107 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f\r\n"
-      );
-    } else {
-      sdp = sdp.replace(
-        "a=rtcp-fb:107 goog-remb\r\n",
-        "a=rtcp-fb:107 goog-remb\r\na=rtcp-fb:107 transport-cc\r\na=fmtp:107 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f\r\n"
-      );
-    }
-
-    resp.jsep.sdp = sdp;
-
-    debug("sub sending answer and setting remote/local description");
-    await Promise.all([
-      peerConnection.setRemoteDescription(resp.jsep),
-      peerConnection
-        .createAnswer()
-        .then(answer =>
-          Promise.all([
-            peerConnection.setLocalDescription(answer),
-            handle.sendJsep(answer)
-          ])
-        )
-    ]);
+    this.negotiateSubscriberMedia(conn, handle, resp.jsep).catch(err => error("Error negotiating media: %o", err));
 
     debug("sub waiting for webrtcup");
-    await this.getWebRtcUpPromise(handle.id).promise;
+    await new Promise(resolve => handle.on("webrtcup", resolve));
 
     // Get the occupant's audio stream.
-    var streams = peerConnection.getRemoteStreams();
+    var streams = conn.getRemoteStreams();
     var mediaStream = streams.length > 0 ? streams[0] : null;
 
     debug("subscriber ready");
     return {
       handle,
       mediaStream,
-      peerConnection
+      conn
     };
   }
 
-  sendJoin(handle, roomId, userId, subscribe) {
+  sendJoin(handle, subscribe) {
     return handle.sendMessage({
       kind: "join",
-      room_id: roomId,
-      user_id: userId,
+      room_id: this.room,
+      user_id: this.userId,
       subscribe
     });
   }
@@ -353,20 +292,16 @@ class JanusAdapter {
     }
   }
 
-  shouldStartConnectionTo(clientId) {
+  shouldStartConnectionTo(client) {
     return true;
   }
 
-  startStreamConnection(clientId) {}
+  startStreamConnection(client) {}
 
-  closeStreamConnection(clientId) {}
+  closeStreamConnection(client) {}
 
   getConnectStatus(clientId) {
-    if (this.occupants[clientId]) {
-      return NAF.adapters.IS_CONNECTED;
-    } else {
-      return NAF.adapters.NOT_CONNECTED;
-    }
+    return this.occupants[clientId] ? NAF.adapters.IS_CONNECTED : NAF.adapters.NOT_CONNECTED;
   }
 
   async updateTimeOffset() {
@@ -421,6 +356,15 @@ class JanusAdapter {
         this.pendingMediaRequests.get(clientId).promise = promise;
       }
       return this.pendingMediaRequests.get(clientId).promise;
+    }
+  }
+
+  setMediaStream(clientId, stream) {
+    this.mediaStreams[clientId] = stream;
+
+    // Resolve the promise for the user's media stream if it exists.
+    if (this.pendingMediaRequests.has(clientId)) {
+      this.pendingMediaRequests.get(clientId).resolve(stream);
     }
   }
 
