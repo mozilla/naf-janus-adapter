c1a4ff699dd863856135bd5e9b1711ec561a5d67
Marshall Quander <marshall@quander.me>
2018-07-03 16:33:05 -0700

    Update build to use webpack 4

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,9 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
+  mode: "production",
   output: {
     filename: "naf-janus-adapter.min.js"
   },
-  devtool: "source-map",
-  plugins: [new MinifyPlugin()]
+  devtool: "source-map"
 });
