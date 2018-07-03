05363b6ef4cf19725ce3e8df904306061e42c419
Marshall Quander <marshall@quander.me>
2018-07-03 16:35:46 -0700

    Toast babel-minify-webpack-plugin
    Webpack 4's built-in UglifyJsPlugin works fine.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
