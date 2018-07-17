8dc8231c5b70ca9702008d7a849a968a69fe9439
Marshall Quander <marshall@quander.me>
2018-07-17 14:54:10 -0700

    Apply workaround for UglifyJsPlugin bug

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "naf-janus-adapter.min.js"
   },
-  devtool: "source-map"
+  devtool: "source-map",
+
+  // necessary due to https://github.com/visionmedia/debug/issues/547
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ uglifyOptions: { compress: { collapse_vars: false }}})
+    ]
+  }
 });
